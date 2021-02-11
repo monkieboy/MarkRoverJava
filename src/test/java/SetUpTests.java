df26d6f4eedf5e32fba47c310c2205e5676e6c33
Mark <mark.gray@magsolutionslimited.co.uk>
2021-02-11 15:01:43 +0000

    initial

---
+++ src/test/java/SetUpTests.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SetUpTests {
+    @Test
+    public void testOne()
+    {
+        assertEquals(true, true);
+    }
+}
